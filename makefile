693b2cbf80f2626ebe0f0ef5ecb40d005748c826
chattrintun <chattrin.tun@student.mahidol.edu>
2025-11-24 10:40:20 +0700

    Initial commit

---
+++ makefile
@@ -0,0 +1,10 @@
+compile: ll_main.cpp 
+	 g++  ll_main.cpp -o ll
+
+
+run: ll
+	 ./ll
+
+
+clean: ll
+	 rm ll
---
+++ node.h
@@ -0,0 +1,51 @@
+// self-referential structure
+#include <iomanip>
+class Node {
+private:
+  int value;
+  Node *nextPtr;
+ // Node *pPtr; // for doubly linked list
+
+public:
+  Node(int x = 0);
+  ~Node();
+  Node* get_next();
+
+  int get_data();
+  void set_next(Node* t) ; 
+  void print();
+
+  // for doubly linked list
+  /*
+  Node * get_prev();
+  /* create another set_prev function or modify set_next to set previous at the same time
+  void set_prev(Node* t); 
+  */
+};
+
+typedef Node *NodePtr; // synonym for Node*
+
+Node::Node(int x) {
+  value = x;
+  nextPtr = NULL;
+  
+}
+
+Node::~Node() { cout << value << " deleted" << endl; }
+
+NodePtr Node::get_next() { return nextPtr; }
+
+int Node::get_data() { return value; }
+/*
+          previousPtr->set_next(newPtr);
+         newPtr->set_next(currentPtr);
+*/
+void Node::set_next(NodePtr t) {
+       this->nextPtr = t; 
+  /* or use set next to set previous too*/
+      
+}
+
+void Node::print(){
+  cout << setw(3) << value;
+}
