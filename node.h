c5e27ca3442b9d4ba773acd43593d1fa62d9fa0b
chattrintun <chattrin.tun@student.mahidol.edu>
2025-11-24 04:32:21 +0000

    Initial commit

--- node.h
+++ node.h
@@ -1,28 +1,19 @@
-// self-referential structure
+
 #include <iomanip>
 class Node {
 private:
-  int value;
+  Rating data;
   Node *nextPtr;
- // Node *pPtr; // for doubly linked list
+ // *nextPtr is next; 
+ // Rating_data holds the rating information
 
 public:
-  Node(int x = 0);
-  ~Node();
-  Node* get_next();
+ 
 
-  int get_data();
-  void set_next(Node* t) ; 
-  void print();
 
-  // for doubly linked list
-  /*
-  Node * get_prev();
-  /* create another set_prev function or modify set_next to set previous at the same time
-  void set_prev(Node* t); 
-  */
+
 };
-
+/*
 typedef Node *NodePtr; // synonym for Node*
 
 Node::Node(int x) {
@@ -36,16 +27,17 @@ Node::~Node() { cout << value << " deleted" << endl; }
 NodePtr Node::get_next() { return nextPtr; }
 
 int Node::get_data() { return value; }
-/*
+
           previousPtr->set_next(newPtr);
          newPtr->set_next(currentPtr);
-*/
+
 void Node::set_next(NodePtr t) {
        this->nextPtr = t; 
-  /* or use set next to set previous too*/
+   or use set next to set previous too
       
 }
 
 void Node::print(){
   cout << setw(3) << value;
 }
+*/
